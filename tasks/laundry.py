7f430277f1bcd773e1a836385b7e17cd6535636a
Xiangbin Hu <hu.xgbn@gmail.com>
2016-10-09 17:57:49 -0500

    test laundry

--- tasks/laundry.py
+++ tasks/laundry.py
@@ -16,9 +16,39 @@ def laundry_update():
     request_url = "http://23.23.147.128/homes/mydata/urba7723"
     response = urllib2.urlopen(request_url)
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
-#    st = json.dumps(json.load(response))
-#r.set('app.tasks.laundry', st)
-#print str(json.load(response))
+    st = json.dumps(json.load(response))
+    r.set('app.tasks.laundry', st)
 
-#output = json.loads(r.get('app.tasks.laundry'))
-#print output
+    data = json.loads(st)
+    length = len(data['location']['rooms'])
+        
+    id_str = ["" for x in range(length)]
+    machine = [ dict() for x in range(length)]
+    name = ["" for x in range(length)]
+    network = ["" for x in range(length)]
+    i = 0
+
+    while(i < length):
+        id_str[i] = data['location']['rooms'][i]['id']
+        machine[i] = data['location']['rooms'][i]['machines']
+        name[i] = data['location']['rooms'][i]['name']
+        network[i] = data['location']['rooms'][i]['networked']
+        i = i + 1
+
+
+    somelist = []
+    i = 0
+    while(i < length):
+        temp = {}
+        temp['machine'] = machine[i]
+        temp['name'] = name[i]
+        temp['networked'] = network[i]
+        somelist.append(json.dumps(temp))
+        i = i + 1
+        
+    i = 0
+ 
+    while(i < length):
+        r.set(id_str[i], somelist[i])
+        i = i + 1
+
